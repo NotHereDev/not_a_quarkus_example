96ccd86d7fbc83a536200d7b8b9ed0bb99692bbc
Florian RICHER <florian.richer@unova.fr>
2024-01-04 21:14:52 +0100

    Update

--- src/main/kotlin/fr/not_here_dev/controllers/UsersController.kt
+++ src/main/kotlin/fr/not_here_dev/controllers/UsersController.kt
@@ -1,6 +1,7 @@
 package fr.not_here_dev.controllers
 
 import fr.not_here_dev.entities.User
+import jakarta.transaction.Transactional
 import jakarta.validation.Valid
 import jakarta.ws.rs.DELETE
 import jakarta.ws.rs.GET
@@ -20,6 +21,7 @@ class UsersController {
     fun show(id: Long) = User.findById(id)
 
     @POST
+    @Transactional
     fun create(@Valid newUser: User): User {
         newUser.persistAndFlush()
         return newUser
@@ -27,18 +29,19 @@ class UsersController {
 
     @Path("/{id}")
     @PATCH
-    fun update(id: Long, @Valid user: User): User {
-        val u = User.findById(id)!!
-        user.id = u.id
+    @Transactional
+    fun update(id: Long, @Valid newUser: User): User {
+        val user = User.findById(id)!!
+        user.login = newUser.login
         user.persistAndFlush()
         return user
     }
 
     @Path("/{id}")
     @DELETE
+    @Transactional
     fun destroy(id: Long): Response {
         User.deleteById(id)
         return Response.ok().build()
     }
-
 }
