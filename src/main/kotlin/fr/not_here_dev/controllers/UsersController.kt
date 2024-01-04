0b455d5b85eda5a035dd82b97886d00ca8393249
Florian RICHER <florian.richer@unova.fr>
2024-01-04 20:54:37 +0100

    Add update + destroy

--- src/main/kotlin/fr/not_here_dev/controllers/UsersController.kt
+++ src/main/kotlin/fr/not_here_dev/controllers/UsersController.kt
@@ -2,9 +2,12 @@ package fr.not_here_dev.controllers
 
 import fr.not_here_dev.entities.User
 import jakarta.validation.Valid
+import jakarta.ws.rs.DELETE
 import jakarta.ws.rs.GET
+import jakarta.ws.rs.PATCH
 import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
+import jakarta.ws.rs.core.Response
 
 @Path("/users")
 class UsersController {
@@ -20,6 +23,22 @@ class UsersController {
     fun create(@Valid newUser: User): User {
         newUser.persistAndFlush()
         return newUser
+    }
+
+    @Path("/{id}")
+    @PATCH
+    fun update(id: Long, @Valid user: User): User {
+        val u = User.findById(id)!!
+        user.id = u.id
+        user.persistAndFlush()
+        return user
+    }
+
+    @Path("/{id}")
+    @DELETE
+    fun destroy(id: Long): Response {
+        User.deleteById(id)
+        return Response.ok().build()
     }
 
 }
