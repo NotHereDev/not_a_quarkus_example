5862b0263ac89ee2b04186dddb99ba8806b43dc6
Florian RICHER <florian.richer@unova.fr>
2024-01-04 23:05:54 +0100

    Add ConstraintViolationMapper

---
+++ src/main/kotlin/fr/not_here_dev/exception_mappers/ConstraintViolationMapper.kt
@@ -0,0 +1,13 @@
+package fr.not_here_dev.exception_mappers
+
+import jakarta.validation.ConstraintViolationException
+import jakarta.ws.rs.core.Response.*
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper
+
+class ConstraintViolationMapper {
+    @ServerExceptionMapper
+    fun mapConstraintViolation(exception: ConstraintViolationException) = status(Status.BAD_REQUEST)
+            .entity(exception.constraintViolations)
+            .build()
+
+}
