c8f3b16de17ed2993a274d5f4316e4df28836ad3
william.richer <william.richer@unova.fr>
2024-01-04 20:40:54 +0100

    ee

--- src/main/kotlin/fr/not_here_dev/controllers/HomeController.kt
+++ src/main/kotlin/fr/not_here_dev/controllers/HomeController.kt
@@ -1,12 +1,21 @@
 package fr.not_here_dev.controllers
 
+import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
+import jakarta.ws.rs.core.Response
+import java.net.URI
 
 @Path("/")
 class RootController{
-
+    @GET
+    fun get() = Response
+            .status(Response.Status.MOVED_PERMANENTLY)
+            .header("Location", "/home")
+            .build()
 }
 
 @Path("/home")
 class HomeController {
+    @GET
+    fun get() = "Wesh alors"
 }
