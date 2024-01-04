bc2be6ce3610bf79a35a99f1d1389130703ec442
Florian RICHER <florian.richer@unova.fr>
2024-01-04 20:46:13 +0100

    Add show + create

--- src/main/kotlin/fr/not_here_dev/controllers/UsersController.kt
+++ src/main/kotlin/fr/not_here_dev/controllers/UsersController.kt
@@ -1,14 +1,25 @@
 package fr.not_here_dev.controllers
 
 import fr.not_here_dev.entities.User
+import jakarta.validation.Valid
 import jakarta.ws.rs.GET
+import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
 
 @Path("/users")
 class UsersController {
 
     @GET
-    fun index(): List<User> {
-        return User.listAll()
+    fun index() = User.listAll()
+
+    @Path("{id}")
+    @GET
+    fun show(id: Long) = User.findById(id)
+
+    @POST
+    fun create(@Valid newUser: User): User {
+        newUser.persistAndFlush()
+        return newUser
     }
+
 }
